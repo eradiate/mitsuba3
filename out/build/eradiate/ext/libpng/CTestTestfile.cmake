975a96365a24028c2a9bf7768c51147ab37647da
PolyRocketMatt <rocketpencil.studios@gmail.com>
2025-07-14 14:20:09 +0200

    Remove .cache and out/

--- out/build/eradiate/ext/libpng/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/matt/rayference/eradiate/ext/mitsuba/ext/libpng
-# Build directory: /home/matt/rayference/eradiate/ext/mitsuba/out/build/eradiate/ext/libpng
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
