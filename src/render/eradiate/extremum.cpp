cb2cc09c02a9dd161f95280dd7f39d5e4b51db27
nicolae marton <nicolae.marton@gmail.com>
2026-08-05 16:33:32 +0200

    Refactor the update interface of the extremum structures to have entry points the update the scale, bbox and structure rebuild.

--- src/render/eradiate/extremum.cpp
+++ src/render/eradiate/extremum.cpp
@@ -4,14 +4,30 @@
 NAMESPACE_BEGIN(mitsuba)
 
 MI_VARIANT ExtremumStructure<Float, Spectrum>::ExtremumStructure()
-    : JitObject<ExtremumStructure>(""){
+    : JitObject<ExtremumStructure>(""), m_scale(1.f) {
 }
 
 MI_VARIANT ExtremumStructure<Float, Spectrum>::ExtremumStructure(const Properties &props)
-    : JitObject<ExtremumStructure>(props.id()){
+    : JitObject<ExtremumStructure>(props.id()), m_scale(1.f) {
 }
 
 MI_VARIANT ExtremumStructure<Float, Spectrum>::~ExtremumStructure() {
+}
+
+MI_VARIANT void ExtremumStructure<Float, Spectrum>::update_extremum(
+    const ScalarBoundingBox3f &bbox, const Volume *volume,
+    std::optional<ScalarFloat> scale) {
+    // set scale if provided
+    if (scale)
+        set_scale(scale.value());
+    // set validity bbox
+    set_bbox(bbox);
+
+    if (!m_bbox.valid())
+        Throw("ExtremumStructure::update_extremum() called with an invalid bbox.");
+
+    // rebuild the extremum structure
+    build(volume);
 }
 
 MI_VARIANT
